fcd20989d3c1ba59749c043a6292c7cad55b5e1f
srikanthBezawada <srikanth.5283@gmail.com>
2015-11-13 01:57:18 +0530

    Adds the boiler plate for this Cytoscape app

---
+++ MClique/src/main/java/org/cytoscape/mclique/internal/logic/CliqueThread.java
@@ -0,0 +1,28 @@
+package org.cytoscape.mclique.internal.logic;
+
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.view.model.CyNetworkView;
+
+public class CliqueThread extends Thread{
+    
+    public CyNetwork currentnetwork;
+    public CyNetworkView currentnetworkview;
+    public CliqueThread(CyNetwork currentnetwork, CyNetworkView currentnetworkview){
+        this.currentnetwork = currentnetwork;
+        this.currentnetworkview = currentnetworkview;
+    }
+    
+    public void run(){
+ 
+        
+        
+        
+        
+        
+        
+    }
+    
+ 
+    
+    
+}
