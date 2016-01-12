14fa1d250478c246b53b69674f734266bdaf27a7
srikanthBezawada <srikanth.5283@gmail.com>
2016-01-12 23:25:20 +0530

    Synchronizing network names, creation

--- MClique/src/main/java/org/cytoscape/mclique/internal/NetworkEventsListener.java
+++ MClique/src/main/java/org/cytoscape/mclique/internal/NetworkEventsListener.java
@@ -1,5 +1,7 @@
 package org.cytoscape.mclique.internal;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.DefaultComboBoxModel;
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.events.NetworkAddedEvent;
@@ -9,6 +11,11 @@ import org.cytoscape.model.events.NetworkDestroyedListener;
 
 public class NetworkEventsListener implements NetworkAddedListener, NetworkDestroyedListener{
     public void handleEvent(NetworkAddedEvent e){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(NetworkEventsListener.class.getName()).log(Level.SEVERE, null, ex);
+        }
         CyNetwork net = e.getNetwork();
         String title = net.getRow(net).get(CyNetwork.NAME, String.class);
         CliqueUI menu = CliqueCore.getTiDieStartMenu();
